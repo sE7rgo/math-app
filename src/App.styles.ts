84c45677d59b33d1d9b524df61de4160c3698b8c
sergii gorbatyi <se7rgo@gmail.com>
2026-01-24 15:12:08 -0800

    Add App styles

---
+++ src/App.styles.ts
@@ -0,0 +1,8 @@
+import type { SxProps, Theme } from '@mui/material';
+
+export const rootContainer: SxProps<Theme> = {
+  maxWidth: '1280px',
+  margin: '0 auto',
+  padding: '2rem',
+  textAlign: 'center',
+};
